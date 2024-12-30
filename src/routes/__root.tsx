2aed03436c436df99f34454664adff76d3188faa
Tomáš Filo <tfilosk@gmail.com>
2024-12-30 14:31:19 +0100

    Hide devtools in prod

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,10 +1,22 @@
 import { Outlet, createRootRouteWithContext } from '@tanstack/react-router';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import { TanStackRouterDevtools } from '@tanstack/router-devtools';
 
 import type { QueryClient } from '@tanstack/react-query';
 import { Container } from '@mui/material';
 import Navigation from '../components/navigation/Navigation';
+import React from 'react';
+
+const TanStackRouterDevtools =
+    process.env.NODE_ENV === 'production'
+        ? () => null // Render nothing in production
+        : React.lazy(() =>
+              // Lazy load in development
+              import('@tanstack/router-devtools').then((res) => ({
+                  default: res.TanStackRouterDevtools
+                  // For Embedded Mode
+                  // default: res.TanStackRouterDevtoolsPanel
+              }))
+          );
 
 export const Route = createRootRouteWithContext<{
     queryClient: QueryClient;
